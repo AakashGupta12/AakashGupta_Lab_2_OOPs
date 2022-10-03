462d80d8dc97817e86547a6398fa77fe7087e300
AakashGupta12 <90550826+AakashGupta12@users.noreply.github.com>
2022-10-03 23:40:54 +0530

    Add files via upload

---
+++ PizzaPasta.java
@@ -0,0 +1,29 @@
+package javaExamples;
+import java.util.*;
+
+class Pizza{
+	String size_LARGE_MEDIUM_SMALL;
+	String crust_THICK_THIN;
+	String type_VEG_NONVEG_VEGAN;
+	String topping_;
+}
+
+class Pasta{
+	String flavour;
+	String type;
+}
+
+class Kitchen{
+	Queue<Orders> q=new LinkedList<Orders>();
+	
+}
+
+class Orders{
+	
+}
+
+public class PizzaPasta {
+	public static void main(String[] args) {
+		
+	}
+}
